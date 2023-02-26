31a3a62334d5fdd75b5a51379f94619137b18b11
Gaétan <g.dumas@etudiant.ecolemelies.fr>
2023-02-26 23:16:00 +0100

    testMove

---
+++ Assets/Script/SC_ControllersTest.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SC_ControllersTest : MonoBehaviour
+{
+
+    public Controllers playerInput;
+    private Vector2 moveInput;
+    private Rigidbody rb;
+    [SerializeField] private float speed;
+    
+    void Start()
+    {
+        playerInput = new Controllers();
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #region Movement
+
+    public void OnMove(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            Debug.Log("$$$$$$$");
+            moveInput = value.Get<Vector2>();
+            rb.velocity = moveInput * speed;
+        }
+    }
+
+    #endregion
+}
